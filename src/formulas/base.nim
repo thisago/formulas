7969f8b05a4eb8f7eb28b8f92efa308982b7d38a
Thisago <thiago@oxyoy.com>
2021-06-17 09:43:25 -0300

    Added configurable PI value

--- src/formulas/base.nim
+++ src/formulas/base.nim
@@ -3,7 +3,7 @@
   :Email: thiago@oxyoy.com
 
   **Created at:** 06/15/2021 10:16:42 Tuesday
-  **Modified at:** 06/16/2021 09:59:09 AM Wednesday
+  **Modified at:** 06/17/2021 09:43:05 AM Thursday
 
   ------------------------------------------------------------------------------
 
--- src/formulas/cone.nim
+++ src/formulas/cone.nim
@@ -3,7 +3,7 @@
   :Email: thiago@oxyoy.com
 
   **Created at:** 06/16/2021 09:57:16 Wednesday
-  **Modified at:** 06/16/2021 11:33:46 AM Wednesday
+  **Modified at:** 06/17/2021 09:43:18 AM Thursday
 
   ------------------------------------------------------------------------------
 
@@ -21,17 +21,19 @@ import math
 type
   Cone = object of Shape
     slantVal*, totalAreaVal*, heightVal*, radiusVal*, volumeVal*: float
+    pi*: float ## Convigurable PI value
 
 # Init
 
-func initCone*(slant, totalArea, height, radius, volume: SomeNumber = 0): Cone =
+func initCone*(slant, totalArea, height, radius, volume: SomeNumber = 0; pi = PI): Cone =
   ## Creates new Cone
   Cone(
     slantVal: float slant,
     totalAreaVal: float totalArea,
     heightVal: float height,
     radiusVal: float radius,
-    volumeVal: float volume
+    volumeVal: float volume,
+    pi: float pi,
   )
 
 # Setters
@@ -51,42 +53,49 @@ func slant*(self: Cone): float =
   sqrt(pow(self.heightVal, 2) + pow(self.radiusVal, 2))
 
 func sideArea*(self: Cone): float =
-  ## Calculates the cone side area with slant height and radius
+  ## Calculates the cone side area (cm2) with slant height and radius
   doAssert self.radiusVal != 0, "No radius provided"
   var slantVal = self.slantVal
   if slantVal == 0:
     slantVal = self.slant()
-  PI * self.radiusVal * slantVal
+  self.pi * self.radiusVal * slantVal
 
 func totalArea*(self: Cone): float =
-  ## Calculates the cone total area with slant height and radius
+  ## Calculates the cone total area (cm2) with slant height and radius
   doAssert self.radiusVal != 0, "No radius provided"
   var slantVal = self.slantVal
   if slantVal == 0:
     slantVal = self.slant()
-  PI * self.radiusVal * (self.radiusVal + slantVal)
+  self.pi * self.radiusVal * (self.radiusVal + slantVal)
 
 func volume*(self: Cone): float =
   ## Calculates the cone volume with height and radius
   doAssert self.heightVal != 0, "No height provided"
   doAssert self.radiusVal != 0, "No radius provided"
-  (PI * pow(self.radiusVal, 2) * self.heightVal) / 3
+  (self.pi * pow(self.radiusVal, 2) * self.heightVal) / 3
 
 func radius*(self: Cone): float =
   ## Calculates the cone radius with height and volume
   doAssert self.heightVal != 0, "No height provided"
   doAssert self.volumeVal != 0, "No volume provided"
-  sqrt((3 * self.volumeVal) / (PI * self.heightVal))
+  sqrt((3 * self.volumeVal) / (self.pi * self.heightVal))
 
 func height*(self: Cone): float =
   ## Calculates the cone height with height and volume
   doAssert self.radiusVal != 0, "No radius provided"
   doAssert self.volumeVal != 0, "No volume provided"
-  (self.volumeVal * 3) / (PI * pow(self.radiusVal, 2))
+  (self.volumeVal * 3) / (self.pi * pow(self.radiusVal, 2))
 
 
 when isMainModule:
   var cone = initCone(height = 10, radius = 8)
+
+  echo cone.slant # 12.8
+  echo cone.sideArea # 321.6
+  echo cone.totalArea # 522.7
+  echo cone.volume # 670.2
+
+  cone.pi = 3.14
 
   echo cone.slant # 12.8
   echo cone.sideArea # 321.6
